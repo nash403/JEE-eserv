fe8b73e40e07045902990c86381ffefb42b00f99
nash403 <nash403@outlook.fr>
2016-12-16 10:14:24 +0100

    create skeleton of StructureDAO interface & impl

---
+++ src/main/java/fr/eservices/structure/dao/StructureDAOImpl.java
@@ -0,0 +1,45 @@
+package fr.eservices.structure.dao;
+
+import java.util.List;
+
+import fr.eservices.structure.model.Structure;
+
+public class StructureDAOImpl implements StructureDAO {
+
+	@Override
+	public List<Structure> listAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Structure> listByRegion(String region_name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Structure findById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void create(Structure struct) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Structure struct) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(long id) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
