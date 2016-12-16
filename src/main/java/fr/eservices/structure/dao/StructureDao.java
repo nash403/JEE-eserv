9d4810b5e3b9c04329bc2a5cc7f391d8591847e2
nash403 <nash403@outlook.fr>
2016-12-16 11:17:07 +0100

    refactor(): class names DAO -> Dao

--- src/main/java/fr/eservices/structure/dao/StructureDao.java
+++ src/main/java/fr/eservices/structure/dao/StructureDao.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import fr.eservices.structure.model.Structure;
 
-public interface StructureDAO {
+public interface StructureDao {
 	
 	public List<Structure> listAll();
 	public List<Structure> listByRegion(String region_name);
