2792cf13ae647ba4ea43fdb4605fbfc8c69d8c45
nash403 <nash403@outlook.fr>
2017-01-02 14:53:00 +0100

    add service for structure

--- src/main/java/fr/eservices/structure/srv/StructureServiceImpl.java
+++ src/main/java/fr/eservices/structure/srv/StructureServiceImpl.java
@@ -1,5 +1,26 @@
 package fr.eservices.structure.srv;
 
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import fr.eservices.structure.dao.StructureDao;
+import fr.eservices.structure.dao.StructureDaoFactory;
+import fr.eservices.structure.model.Structure;
+
+@Component
 public class StructureServiceImpl implements StructureService {
+	
+	StructureDao dao = StructureDaoFactory.createStructureDao();
+		
+	@Override
+	public List<Structure> findAll() {
+		return dao.listAll();
+	}
+
+	@Override
+	public void create(Structure struct) {
+		dao.create(struct);
+	}
 	
 }
