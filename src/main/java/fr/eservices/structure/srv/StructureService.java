c6a534e53e8acd2e243c76c930ea0461c215aa15
nash403 <nash403@outlook.fr>
2017-01-02 14:53:00 +0100

    add service for structure

--- src/main/java/fr/eservices/structure/srv/StructureService.java
+++ src/main/java/fr/eservices/structure/srv/StructureService.java
@@ -1,5 +1,12 @@
 package fr.eservices.structure.srv;
 
+import java.util.List;
+
+import fr.eservices.structure.model.Structure;
+
 public interface StructureService {
+	
+	public List<Structure> findAll();
+	public void create(Structure struct);
 	
 }
