bb27619226abbf820ab81345c0903c06d4496b5e
ThibautBrnl <thibaut.brunel@outlook.fr>
2017-01-04 13:27:43 +0100

    Excluded the .DS_Store files + removed unused imports

--- src/main/java/fr/eservices/structure/dao/StructureDaoImpl.java
+++ src/main/java/fr/eservices/structure/dao/StructureDaoImpl.java
@@ -3,9 +3,6 @@ package fr.eservices.structure.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Component;
