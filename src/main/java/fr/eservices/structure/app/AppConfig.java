90366590956af9441874de2bc876fe29db6f14eb
nash403 <nash403@outlook.fr>
2017-01-04 19:11:27 +0100

    refactor(): rename H2 db filename

--- src/main/java/fr/eservices/structure/app/AppConfig.java
+++ src/main/java/fr/eservices/structure/app/AppConfig.java
@@ -59,7 +59,7 @@ public class AppConfig {
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("org.h2.Driver");
-		dataSource.setUrl("jdbc:h2:./structDb");
+		dataSource.setUrl("jdbc:h2:~/jee-eservFA/structureSrv-db");
 		return dataSource;
 	}
 
