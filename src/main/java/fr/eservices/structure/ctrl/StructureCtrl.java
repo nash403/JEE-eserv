10c081cbdefab2950f3765748f998d1a0ef5686e
nash403 <nash403@outlook.fr>
2017-01-04 20:28:26 +0100

    list all structures in view

--- src/main/java/fr/eservices/structure/ctrl/StructureCtrl.java
+++ src/main/java/fr/eservices/structure/ctrl/StructureCtrl.java
@@ -1,10 +1,13 @@
 package fr.eservices.structure.ctrl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import fr.eservices.structure.model.Structure;
 import fr.eservices.structure.srv.StructureServiceImpl;
 
 @Controller
